4cb2ab40c2a84c3849ab6a0edcd63fd02d22dffa
Levignoble <kshitijgupta070704@gmail.com>
2025-03-11 10:09:25 +0530

    sedconde

---
+++ frontend/a.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout << "setting up the environment" << endl;
+    cout << "installing dependencies" << endl;
+    cout << "running the application" << endl;
+    return 0;
+}
